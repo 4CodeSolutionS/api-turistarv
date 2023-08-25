c229b34be36d57871f0657b02bde4247bf38d8d6
Kaio Moreira <kaio-dev@outlook.com>
2023-08-25 11:52:21 -0300

    import repository ehtreal

--- src/script/sendEmailFake.ts
+++ src/script/sendEmailFake.ts
@@ -1,42 +1,17 @@
-import nodemailer from 'nodemailer';
+import { EtherealProvider } from "@/providers/MailProvider/implementations/provider-ethereal";
 
-// Generate SMTP service account from ethereal.email
-nodemailer.createTestAccount((err, account) => {
-    if (err) {
-        console.error('Failed to create a testing account. ' + err.message);
-        return process.exit(1);
-    }
+async function run() {
+const ehtreal = new EtherealProvider();
 
-    console.log('Credentials obtained, sending message...');
+    const pathTemplate = './src/views/emails/forgot-password.hbs'
+    await ehtreal.sendEmail(
+       ' findUserByEmail.email', 
+        'findUserByEmail.name', 
+        'Redefinição de Senha', 
+        'link', 
+        pathTemplate 
+    );
 
-    // Create a SMTP transporter object
-    let transporter = nodemailer.createTransport({
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-            user: account.user,
-            pass: account.pass
-        }
-    });
+}
 
-    // Message object
-    let message = {
-        from: 'Sender Name <sender@example.com>',
-        to: 'Recipient <recipient@example.com>',
-        subject: 'Nodemailer is unicode friendly ✔',
-        text: 'Hello to myself!',
-        html: '<p><b>Hello</b> to myself!</p>'
-    };
-
-    transporter.sendMail(message, (err, info) => {
-        if (err) {
-            console.log('Error occurred. ' + err.message);
-            return process.exit(1);
-        }
-
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
-});
+run();
